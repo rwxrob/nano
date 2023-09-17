c30e54b49632f93561b7b86f6382248bae1bfc36
Replit user <>
2023-09-17 12:31:29 +0000

    Refactoring...

---
+++ msg.h
@@ -0,0 +1,30 @@
+char nano1[] =  
+"[0;31m                                        [1;31m NNNNNNNN        NNNNNNNN     OOOOOOOOO      !!!\n"
+"[0;31m                                        [1;31m N:::::::N       N::::::N   OO:::::::::OO   !!:!!\n"
+"[0;31m                                        [1;31m N::::::::N      N::::::N OO:::::::::::::OO !:::!\n"
+"[0;31m                                        [1;31m N:::::::::N     N::::::NO:::::::OOO:::::::O!:::!\n"
+"[0;31mnnnn  nnnnnnnn      aaaaaaaaaaaaa       [1;31m N::::::::::N    N::::::NO::::::O   O::::::O!:::!\n"
+"[0;31mn:::nn::::::::nn    a::::::::::::a      [1;31m N:::::::::::N   N::::::NO:::::O     O:::::O!:::!\n"
+"[0;31mn::::::::::::::nn   aaaaaaaaa:::::a     [1;31m N:::::::N::::N  N::::::NO:::::O     O:::::O!:::!\n"
+"[0;31mnn:::::::::::::::n           a::::a     [1;31m N::::::N N::::N N::::::NO:::::O     O:::::O!:::!\n"
+"[0;31m  n:::::nnnn:::::n    aaaaaaa:::::a     [1;31m N::::::N  N::::N:::::::NO:::::O     O:::::O!:::!\n"
+"[0;31m  n::::n    n::::n  aa::::::::::::a     [1;31m N::::::N   N:::::::::::NO:::::O     O:::::O!:::!\n"
+"[0;31m  n::::n    n::::n a::::aaaa::::::a     [1;31m N::::::N    N::::::::::NO:::::O     O:::::O!!:!!\n"
+"[0;31m  n::::n    n::::na::::a    a:::::a     [1;31m N::::::N     N:::::::::NO::::::O   O::::::O !!! \n"
+"[0;31m  n::::n    n::::na::::a    a:::::a     [1;31m N::::::N      N::::::::NO:::::::OOO:::::::O     \n"
+"[0;31m  n::::n    n::::na:::::aaaa::::::a     [1;31m N::::::N       N:::::::N OO:::::::::::::OO  !!! \n"
+"[0;31m  n::::n    n::::n a::::::::::aa:::a    [1;31m N::::::N        N::::::N   OO:::::::::OO   !!:!!\n"
+"[0;31m  nnnnnn    nnnnnn  aaaaaaaaaa  aaaa    [1;31m NNNNNNNN         NNNNNNN     OOOOOOOOO      !!! \n\n"
+"                      [0;36mBut seriously, learn vi (not even vim).\n"
+"                         You will be a better developer\n"
+"                             and systems engineer.[0m\n\n";
+
+char nano1s[] = 
+"       [0;31mna [0;31mNOOOO!\n\n"
+"[0;36m      But seriously, \n"
+" learn vi (not even vim). \n"
+"   You will be a better \n"
+"  developer and engineer.[0m \n";
+
+
+
